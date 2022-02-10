1af835ca52e4a8fbf69debf29b931ea5b0852a8a
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2022-02-09 20:01:55 -0500

    string class

--- 07_strings.playground/Contents.swift
+++ 07_strings.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
+let greeting = "Hello, playground"
+let multiLineString = """
+    I'm Phillipe
+    this is my swift course\
+    Peace
+"""
 
+let wiseWords = "\"Time is the real coin\" - your momma"
+
+let dollarSign = "\u{24}"
+let blackHeart = "\u{2665}"
+let heartEmoji = "\u{1F496}"
+
+var emptyString = ""
+var anotherEmptyString = String()
+if emptyString.isEmpty {
+    print("Nothing to see here")
+} else {
+    print("String has value")
+}
+
+var someString = "A horse"
+someString += " and a carriage"
